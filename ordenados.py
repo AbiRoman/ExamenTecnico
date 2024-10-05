6ff93d6504479cc41fcb0d3eb8092633704a8626
AbiRoman <abigailromanc04@gmail.com>
2024-10-04 19:50:48 -0600

    Delete ordenados.py

--- ordenados.py
+++
@@ -1,9 +0,0 @@
-#1. Escribe una función en Python que reciba una lista de números y devuelva una nueva lista con los números ordenados de menor a mayor.
-def ordenar_lista(numeros):
-    """Recibe una lista de números y devuelve una nueva lista ordenada de menor a mayor."""
-    return sorted(numeros)
-
-# Ejemplo de uso
-numeros = [5, 3, 8, 1, 4]
-lista_ordenada = ordenar_lista(numeros)
-print(lista_ordenada)  # Salida: [1, 3, 4, 5, 8]
